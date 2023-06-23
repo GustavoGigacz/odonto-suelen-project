dbda1bd861c523fbafc014c67a4f062c5ca33247
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-06-23 14:38:07 -0300

    add icons

---
+++ app/components/shared/icons/BurguerIcon.tsx
@@ -0,0 +1,18 @@
+export default function BurguerIcon() {
+  return (
+    <svg
+      width='32'
+      height='32'
+      viewBox='0 0 32 32'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M6.66663 8H25.3333M6.66663 16H25.3333M6.66663 24H25.3333'
+        stroke='#800020'
+        strokeWidth='2'
+        strokeLinecap='round'
+      />
+    </svg>
+  );
+}
