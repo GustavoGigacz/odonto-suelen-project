20c8ee636259d55b5ba30d2549b3852ad30b91f0
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-06-27 16:03:59 -0300

    add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,11 +22,18 @@ module.exports = {
       },
       maxWidth: {
         desktop: "1512px",
+        marks: "1770px",
         tablet: "720px",
         mobile: "328px",
       },
       screens: {
-        mobile: "900px",
+        desk: "900px",
+        customers: "1050px",
+        screenMedian: "1250px",
+        nomark: "1510px",
+      },
+      boxShadow: {
+        box: "0px -0.05534512922167778px 2.2138051986694336px 0px rgba(0, 0, 0, 0.02), 0px -0.13300204277038574px 5.32008171081543px 0px rgba(0, 0, 0, 0.03), 0px -0.25043103098869324px 10.017241477966309px 0px rgba(0, 0, 0, 0.04), 0px -0.4467261731624603px 17.869047164916992px 0px rgba(0, 0, 0, 0.04), 0px -0.8355522155761719px 33.422088623046875px 0px rgba(0, 0, 0, 0.05), 0px -2px 80px 0px rgba(0, 0, 0, 0.07)",
       },
     },
   },
