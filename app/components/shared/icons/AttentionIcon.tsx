a5baec8db1f9461c82ca28d0774e0bbc3bf19c9b
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-07-03 17:11:55 -0300

    add svg

---
+++ app/components/shared/icons/AttentionIcon.tsx
@@ -0,0 +1,40 @@
+export default function AttentionIcon() {
+  return (
+    <svg
+      width='60'
+      height='61'
+      viewBox='0 0 60 61'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g id='Icon'>
+        <rect y='0.181885' width='60' height='60' rx='10' fill='white' />
+        <g id='Group'>
+          <path
+            id='Vector'
+            d='M30 45C38.2843 45 45 38.2843 45 30C45 21.7157 38.2843 15 30 15C21.7157 15 15 21.7157 15 30C15 38.2843 21.7157 45 30 45Z'
+            stroke='#800020'
+            strokeWidth='2.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+          <path
+            id='Vector_2'
+            d='M30 36.6666H30.0167V36.6833H30V36.6666Z'
+            stroke='#800020'
+            strokeWidth='3.75'
+            strokeLinejoin='round'
+          />
+          <path
+            id='Vector_3'
+            d='M30 30V23.3334'
+            stroke='#800020'
+            strokeWidth='2.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
