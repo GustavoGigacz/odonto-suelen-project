add87f81be01a2f9f8d1b5250ac5a0ee4d1dc14c
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-07-06 17:40:37 -0300

    add svgs

---
+++ app/components/home/OurAttendment.tsx
@@ -0,0 +1,47 @@
+import ClockIcon from "../shared/icons/ClockIcon";
+import CorrectIcon from "../shared/icons/CorrectIcon";
+import PlanIcon from "../shared/icons/PlanIcon";
+import Square3DIcon from "../shared/icons/Square3DIcon";
+import ThoothIcon from "../shared/icons/ThoothIcon";
+
+export default function OurAttendment() {
+  return (
+    <div className='customers:mb-60 mb-12 max-w-desktop mx-auto px-8 mt-60'>
+      <h2 className='text-center text-blue1 text-5xl font-roboto font-bold mb-10'>
+        Como é ser nosso cliente?
+      </h2>
+      <ul className='flex screen3:gap-5 gap-4 flex-wrap justify-center screen3:text-2xl text-xl font-archivo font-bold text-blue1 '>
+        <li className='p-9 shadow-box2 rounded-border'>
+          <Square3DIcon />
+          <p className='max-w-[197px] screen3:pt-5 pt-4 '>
+            Avaliação com Raio-X Panorâmico (sem custo)
+          </p>
+        </li>
+        <li className='p-9 shadow-box2 rounded-border'>
+          <PlanIcon />
+          <p className='max-w-[197px] screen3:pt-5 pt-4'>
+            Plano de tratamento e diagnóstico
+          </p>
+        </li>
+        <li className='p-9 shadow-box2 rounded-border '>
+          <ClockIcon />
+          <p className='max-w-[197px] screen3:pt-5 pt-4'>
+            Aprovação e agendamento do procedimento
+          </p>
+        </li>
+        <li className='p-9 shadow-box2 rounded-border'>
+          <ThoothIcon />
+          <p className='max-w-[197px] screen3:pt-5 pt-4'>
+            Realização dos procedimentos dentro da clínica
+          </p>
+        </li>
+        <li className='p-9 shadow-box2 rounded-border'>
+          <CorrectIcon />
+          <p className='max-w-[197px] screen3:pt-5 pt-4'>
+            Cuidados e assistência pré e pós cirúrgica
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ app/components/shared/icons/Square3DIcon.tsx
@@ -0,0 +1,30 @@
+export default function Square3DIcon() {
+  return (
+    <svg
+      width='47'
+      height='52'
+      viewBox='0 0 47 52'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g id='Group'>
+        <path
+          id='Vector'
+          d='M23.5 2.45195L44.5 14.1195V37.3205L23.5 48.9855L2.5 37.3205V14.1195L23.5 2.45195Z'
+          stroke='#800121'
+          strokeWidth='4'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+        <path
+          id='Vector_2'
+          d='M44.0743 37.0224L24.0117 26.2402C23.7941 26.1235 23.5493 26.0622 23.3005 26.0622C23.0516 26.0622 22.8069 26.1235 22.5893 26.2402L2.52661 37.0224M2.59504 14.7298L22.5868 25.4741C22.8047 25.5913 23.0499 25.6528 23.2992 25.6528C23.5486 25.6528 23.7938 25.5913 24.0117 25.4741L44.0743 14.692M23.3005 47.1377V4.57666'
+          stroke='#800121'
+          strokeWidth='4'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      </g>
+    </svg>
+  );
+}
