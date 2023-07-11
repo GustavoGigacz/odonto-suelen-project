8cb0017a7c6475128c61cb81a39b71397822e7ef
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-07-11 15:23:52 -0300

    add responsive Contact

--- app/components/home/ContactUs.tsx
+++ app/components/home/ContactUs.tsx
@@ -4,28 +4,38 @@ import FacebookIcon from "../shared/icons/FacebookIcon";
 import LinkedinIcon from "../shared/icons/LinkedinIcon";
 import TwitterIcon from "../shared/icons/TwitterIcon";
 import UserIcon from "../shared/icons/UserIcon";
+import PhoneIcon from "../shared/icons/PhoneIcon";
+import MessangeIcon from "../shared/icons/MessangeIcon";
 
 export default function ContactUs() {
   return (
     <div className='w-full max-w-desktop px-8 mx-auto flex screen3:flex-row flex-col justify-between screen3:mb-60 mb-12'>
-      <div className=' text-blue1'>
-        <h3 className='text-5xl font-roboto font-bold max-w-[463px] mb-12 '>
+      <div className=' text-blue1 mb-12'>
+        <h3 className='screen4:text-5xl text-3xl font-roboto font-bold max-w-[463px] mb-12 '>
           Entre em Contato Conosco
         </h3>
         <ul>
-          <li className='flex flex-col gap-2 mb-7'>
-            <span className='font-roboto font-bold text-3xl'>Telefone</span>
-            <span className='font-archivo text-2xl'>(42)913245434</span>
+          <li className='flex flex-col screen4:gap-2 mb-7'>
+            <span className='font-roboto font-bold screen4:text-3xl text-xl'>
+              Telefone
+            </span>
+            <span className='font-archivo screen4:text-2xl text-sm'>
+              (42)913245434
+            </span>
           </li>
-          <li className='flex flex-col gap-2 mb-7'>
-            <span className='font-roboto text-3xl font-bold'>Email</span>
-            <Link href={"#"} className='font-archivo text-2xl'>
+          <li className='flex flex-col screen4:gap-2 mb-7'>
+            <span className='font-roboto font-bold screen4:text-3xl text-xl'>
+              Email
+            </span>
+            <Link href={"#"} className='font-archivo screen4:text-2xl text-sm'>
               drasuelen@gmail.com
             </Link>
           </li>
-          <li className='flex flex-col gap-2 mb-7'>
-            <span className='font-roboto text-3xl font-bold'>Localização</span>
-            <span className='font-archivo text-2xl max-w-[320px]'>
+          <li className='flex flex-col screen4:gap-2 mb-7'>
+            <span className='font-roboto font-bold screen4:text-3xl text-xl'>
+              Localização
+            </span>
+            <span className='font-archivo max-w-[320px] screen4:text-2xl text-sm'>
               R. Altíno Pereira de Lima, 1814 - Centro, São Mateus do Sul - PR,
               83900-000
             </span>
@@ -55,23 +65,32 @@ export default function ContactUs() {
         </ul>
       </div>
       <div>
-        <form action='' className='flex flex-col gap-6'>
+        <form action='' className='flex flex-col screeb4:gap-6 gap-[10px] '>
           <div className='flex gap-10 items-center outline-2 outline-black1 outline rounded-[10px] py-4 pl-7 pr-4'>
-            <UserIcon />
+            <span>
+              <UserIcon />
+            </span>
             <input
               type='text'
               className='border-none outline-none text-2xl w-[700px]'
             />
           </div>
           <div className='flex gap-10 items-center outline-2 outline-black1 outline rounded-[10px] py-4 pl-7 pr-4'>
-            <UserIcon />
+            <span>
+              <PhoneIcon />
+            </span>
             <input
               type='text'
               className='border-none outline-none text-2xl w-[700px]'
             />
           </div>
-          <div className='outline-2 outline-black1 outline rounded-[10px] py-4 pl-7 pr-5'>
+          <div className=' outline-2 outline-black1 outline rounded-[10px] py-4 pl-7 pr-5'>
             <textarea className='border-none outline-none text-2xl w-[800px]'></textarea>
+          </div>
+          <div>
+            <button className='bg-red2 screen4:py-3 screen4:px-6 py-2 px-4 rounded-border text-white screen4:text-2xl text-sm font-roboto font-bold'>
+              Enviar Mensagem
+            </button>
           </div>
         </form>
       </div>
