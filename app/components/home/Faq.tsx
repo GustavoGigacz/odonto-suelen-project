198d9676057d206768b56c95a01c325d795cccca
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-07-13 14:45:49 -0300

    add faq responsible

--- app/components/home/Faq.tsx
+++ app/components/home/Faq.tsx
@@ -14,7 +14,7 @@ export default function FaqPage() {
       <div className='border-b border-solid border-blue1'>
         <Accordion type='single' collapsible>
           <AccordionItem value='item-1'>
-            <AccordionTrigger className='font-medium font-roboto text-3xl'>
+            <AccordionTrigger className='font-medium font-roboto screen3:text-3xl text-lg'>
               Quanto tempo demora o tratamento com lentes de contato?
             </AccordionTrigger>
             <AccordionContent className='font-archivo '>
@@ -29,8 +29,8 @@ export default function FaqPage() {
       </div>
       <div className='border-b border-solid border-blue1'>
         <Accordion type='single' collapsible>
-          <AccordionItem value='item-2'>
-            <AccordionTrigger className='font-medium font-roboto text-3xl'>
+          <AccordionItem value='item-1'>
+            <AccordionTrigger className='font-medium font-roboto screen3:text-3xl text-lg'>
               Quanto tempo demora o tratamento com lentes de contato?
             </AccordionTrigger>
             <AccordionContent className='font-archivo '>
@@ -45,40 +45,8 @@ export default function FaqPage() {
       </div>
       <div className='border-b border-solid border-blue1'>
         <Accordion type='single' collapsible>
-          <AccordionItem value='item-3'>
-            <AccordionTrigger className='font-medium font-roboto text-3xl'>
-              Quanto tempo demora o tratamento com lentes de contato?
-            </AccordionTrigger>
-            <AccordionContent className='font-archivo '>
-              A lente de contato dental pode durar de 10 a 20 anos. Mas tenha em
-              mente que esse tempo depende de alguns fatores, como boa higiene
-              bucal, consultas regulares com o dentista, tipo de alimentação
-              adotada após o procedimento, qualidade dos materiais usados, entre
-              outros.
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
-      <div className='border-b border-solid border-blue1'>
-        <Accordion type='single' collapsible>
-          <AccordionItem value='item-4'>
-            <AccordionTrigger className='font-medium font-roboto text-3xl'>
-              Quanto tempo demora o tratamento com lentes de contato?
-            </AccordionTrigger>
-            <AccordionContent className='font-archivo '>
-              A lente de contato dental pode durar de 10 a 20 anos. Mas tenha em
-              mente que esse tempo depende de alguns fatores, como boa higiene
-              bucal, consultas regulares com o dentista, tipo de alimentação
-              adotada após o procedimento, qualidade dos materiais usados, entre
-              outros.
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
-      <div className='border-b border-solid border-blue1'>
-        <Accordion type='single' collapsible>
-          <AccordionItem value='item-5'>
-            <AccordionTrigger className='font-medium font-roboto text-3xl'>
+          <AccordionItem value='item-1'>
+            <AccordionTrigger className='font-medium font-roboto screen3:text-3xl text-lg'>
               Quanto tempo demora o tratamento com lentes de contato?
             </AccordionTrigger>
             <AccordionContent className='font-archivo '>
