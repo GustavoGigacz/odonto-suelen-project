a29c3f06060111f669b70208d25674099a0d5156
Pedro H G Migacz <pedromigacz@gmail.com>
2023-07-24 17:36:42 -0300

    fix alignment

--- app/components/home/OurServices/Service.tsx
+++ app/components/home/OurServices/Service.tsx
@@ -1,4 +1,4 @@
-import Image, { StaticImageData } from "next/image";
+import Image, { StaticImageData } from 'next/image';
 
 interface ServiceProps {
   title: string;
@@ -19,34 +19,38 @@ export default function Service({
 }: ServiceProps) {
   return (
     <>
-      <div className={`w-full ${reversed ? "bg-white" : "bg-red2"}`}>
-        <div
-          className={`flex ${
-            reversed ? "flex-row-reverse" : "flex-row"
-          } max-[1330px]:flex-col  justify-between max-w-desktop mx-auto px-8 pt-24 items-center pb-28`}
-        >
+      <div className={`w-full ${reversed ? 'bg-white' : 'bg-red2'}`}>
+        <div className={`flex`}>
           <div
-            className={`max-w-[665px] mb-4 ${
-              reversed ? "text-blue1" : "text-white"
-            } `}
+            className={`flex ${
+              reversed ? 'flex-row-reverse' : 'flex-row'
+            } max-[1330px]:flex-col items-center max-[1330px]:items-start justify-between max-w-desktop mx-auto px-8 pt-24 pb-28 gap-1 max-[1330px]:w-auto w-full`}
           >
-            <p
-              className={`screen3:text-2xl text-xl font-thin italic font-archivo ${
-                reversed ? "text-red2" : "text-white"
-              }  `}
+            <div
+              className={`max-w-[665px] mb-4 ${
+                reversed ? 'text-blue1' : 'text-white'
+              } `}
             >
-              {upperTitle}
-            </p>
-            <h3 className='screen3:text-5xl text-3xl font-roboto font-bold screen3:mb-10 mb-5 screen3:mt-2 mt-1'>
-              {title}
-            </h3>
-            <p className='screen3:text-xl text-sm font-archivo'>{paragraph}</p>
-            <ul className='screen3:text-xl text-sm font-archivo list-disc pl-5 mt-2'>
-              {/* <li></li> */}
-            </ul>
-          </div>
-          <div className='max-w-[665px]'>
-            <Image src={image} alt={""} />
+              <p
+                className={`screen3:text-2xl text-xl font-thin italic font-archivo ${
+                  reversed ? 'text-red2' : 'text-white'
+                }  `}
+              >
+                {upperTitle}
+              </p>
+              <h3 className='screen3:text-5xl text-3xl font-roboto font-bold screen3:mb-10 mb-5 screen3:mt-2 mt-1'>
+                {title}
+              </h3>
+              <p className='screen3:text-xl text-sm font-archivo'>
+                {paragraph}
+              </p>
+              <ul className='screen3:text-xl text-sm font-archivo list-disc pl-5 mt-2'>
+                {/* <li></li> */}
+              </ul>
+            </div>
+            <div className='max-w-[665px]'>
+              <Image src={image} alt={''} />
+            </div>
           </div>
         </div>
       </div>
